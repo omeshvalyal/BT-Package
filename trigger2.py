882c683eaaa876efd52b45f4e74d110e7515107a
omeshvalyal <omeshvalyal@gmail.com>
2024-01-21 19:45:46 +0530

    git checkout added

--- trigger2.py
+++ trigger2.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     remote_hostname = "54.91.98.173"
     remote_port = 22  # Default SSH port
     remote_username = "ec2-user"
-    private_key_path = "/home/ec2-user/my_key/id_rsa"  # Replace with your private key file path
+    private_key_path = "/var/lib/jenkins/my_key/id_rsa"  # Replace with your private key file path
     remote_script_path = "/home/ec2-user/welcome.sh"
 
     execute_remote_script(remote_hostname, remote_port, remote_username, private_key_path, remote_script_path)
